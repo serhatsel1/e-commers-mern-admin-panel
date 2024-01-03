13998bc7111580c78f4dddcf556189321a608209
Serhat Sel <selserhat01@gmail.com>
2024-01-03 22:30:10 +0300

    header footer ve policy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
-import Header from "./components/Header";
+import Footer from "./components/Layout/Footer";
+import Header from "./components/Layout/Header";
+import Policy from "./components/Layout/Policy";
 
 function App() {
   return (
     <>
       <Header />
       <h1>Hello</h1>
+      <Policy />
+      <Footer />
     </>
   );
 }
