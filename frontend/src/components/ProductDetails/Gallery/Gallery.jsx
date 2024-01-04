d8183805fcbf8916ef89e1de3681f12129a91b04
Serhat Sel <selserhat01@gmail.com>
2024-01-04 21:50:44 +0300

    active image galerry

--- frontend/src/components/ProductDetails/Gallery/Gallery.jsx
+++ frontend/src/components/ProductDetails/Gallery/Gallery.jsx
@@ -1,49 +1,56 @@
+import { useState } from "react";
+import productsData from "../../../data.json";
 import "./Gallery.css";
 
 const Gallery = () => {
+  const [activeİmage, setActiveİmage] = useState(
+    productsData[0].img.singleImage
+  );
+  const [show, setShow] = useState(0);
+
+  const slideRightHandle = () => {
+    setShow((prevState) => (prevState + 1) % 3);
+  };
+
+  const slideLeftHandle = () => {
+    setShow((prevState) => (prevState - 1 + 3) % 3);
+  };
+  console.log(productsData);
   return (
     <div className="product-gallery">
       <div className="single-image-wrapper">
-        <img src="img/products/product2/1.png" id="single-image" alt="" />
+        <img src={activeİmage} id="single-image" alt="" />
       </div>
       <div className="product-thumb">
         <div className="glide__track" data-glide-el="track">
           <ol className="gallery-thumbs glide__slides">
-            <li className="glide__slide glide__slide--active">
-              <img
-                src="img/products/product3/1.png"
-                alt=""
-                className="img-fluid active"
-              />
-            </li>
-
-            <li className="glide__slide">
-              <img
-                src="img/products/product3/2.png"
-                alt=""
-                className="img-fluid"
-              />
-            </li>
-
-            <li className="glide__slide">
-              <img
-                src="img/products/product3/3.png"
-                alt=""
-                className="img-fluid"
-              />
-            </li>
+            {productsData[0].img.thumbs.map((product, i) => (
+              <li
+                key={i}
+                onClick={() => setActiveİmage(product)}
+                className="glide__slide glide__slide--active"
+              >
+                <img
+                  src={`${product}`}
+                  alt=""
+                  className={`img-fluid ${product === activeİmage && "active"}`}
+                />
+              </li>
+            ))}
           </ol>
         </div>
         <div className="glide__arrows" data-glide-el="controls">
           <button
             className="glide__arrow glide__arrow--left"
             data-glide-dir="<"
+            onClick={slideLeftHandle}
           >
             <i className="bi bi-chevron-left"></i>
           </button>
           <button
             className="glide__arrow glide__arrow--right"
             data-glide-dir=">"
+            onClick={slideRightHandle}
           >
             <i className="bi bi-chevron-right"></i>
           </button>
