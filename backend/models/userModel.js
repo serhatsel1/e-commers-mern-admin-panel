d52f2b845850c159d8da509fd8f8d297bd852b2e
Serhat Sel <selserhat01@gmail.com>
2024-01-06 22:03:06 +0300

    usermodel oluşturuldu

---
+++ backend/models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      default: "user",
+      enum: ["user, admin"],
+      avatar: {
+        type: String,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
