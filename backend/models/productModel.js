e72b8f48c3d7cf5dccb743686db25c6a3fe5232f
Serhat Sel <selserhat01@gmail.com>
2024-01-06 23:58:08 +0300

    product model oluşturuldu

---
+++ backend/models/productModel.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+//alternatif bir şekilde oluşturma
+const ReviewSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    price: {
+      current: { type: Number, required: true },
+      discount: { type: Number },
+    },
+    reviews: [ReviewSchema],
+    description: {
+      type: String,
+      required: true,
+    },
+    colors: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    sizes: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
