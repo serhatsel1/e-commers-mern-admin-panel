7d70bd6aa6fece45bad503dcde2a928eec1bcd06
Serhat Sel <selserhat01@gmail.com>
2024-01-06 20:44:16 +0300

    category şeması oluşturuldu

---
+++ backend/models/categoryModal.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Category = mongoose.model("Category", categorySchema);
+
+export default Category;
