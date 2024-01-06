6165050387ca498da7551f84d19d077e377f5daa
Serhat Sel <selserhat01@gmail.com>
2024-01-07 00:19:57 +0300

    tüm ürünleri ve tekli ürünü getime işlemi

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -12,9 +12,43 @@ const createProduct = async (req, res) => {
       newProduct,
     });
   } catch (error) {
-    console.log("createCategory-->", error);
+    console.log("createProduct-->", error);
     res.status(500).json({ message: error });
   }
 };
 
-export { createProduct };
+const allProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json({
+      products,
+    });
+  } catch (error) {
+    console.log("allProducts-->", error);
+    res.status(500).json({
+      message: error,
+    });
+  }
+};
+
+const getSingleProduct = async (req, res) => {
+  try {
+    const productId = req.params.productId;
+
+    const singleProduct = await Product.findById(productId);
+
+    // console.log("singleProduct", singleProduct);
+
+    res.status(200).json({
+      singleProduct,
+    });
+  } catch (error) {
+    console.log("getSingleProduct-->", error);
+    res.status(500).json({
+      message: error,
+    });
+  }
+};
+
+export { createProduct, allProducts, getSingleProduct };
--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createProduct } from "../controllers/products.js";
+import {
+  createProduct,
+  allProducts,
+  getSingleProduct,
+} from "../controllers/products.js";
 
 const router = express.Router();
 
 router.post("/", createProduct);
+router.get("/", allProducts);
+router.get("/:productId", getSingleProduct);
 
 export default router;
