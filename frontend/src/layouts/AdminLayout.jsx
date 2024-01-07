68fe7a9d9b825a48147c682d90be67fae52448a1
Serhat Sel <selserhat01@gmail.com>
2024-01-07 21:24:10 +0300

    ant deisng kurulumu yapıldı

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -1,6 +1,9 @@
+import { Button } from "antd";
+
 const AdminLayout = () => {
   return (
     <div className="admin-layout">
+      <Button type="primary">Primary Button</Button>
       <div>Header</div>
       <div>sidebar</div>
       <div>content</div>
