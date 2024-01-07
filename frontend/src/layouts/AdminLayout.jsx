63ad59e6a022c23e1325feb3fc0e35dd815b6e9a
Serhat Sel <selserhat01@gmail.com>
2024-01-07 21:16:24 +0300

    frontend adminlayout

---
+++ frontend/src/layouts/AdminLayout.jsx
@@ -0,0 +1,11 @@
+const AdminLayout = () => {
+  return (
+    <div className="admin-layout">
+      <div>Header</div>
+      <div>sidebar</div>
+      <div>content</div>
+    </div>
+  );
+};
+
+export default AdminLayout;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,21 @@
 import ReactDOM from "react-dom/client";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import MainLayout from "./layouts/MainLayout.jsx";
+import { Layout } from "./layouts/Layout.jsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
 import CartProvider from "./context/CartProvider.jsx";
 
+
+console.log(location);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <CartProvider>
-      <MainLayout>
+      <Layout>
         <App />
-      </MainLayout>
+      </Layout>
     </CartProvider>
   </BrowserRouter>
 );
