783d6a757a120134a5eb6cb1dcbc07f4f641ae02
Serhat Sel <selserhat01@gmail.com>
2024-01-14 23:14:08 +0300

    Stripe payment frontend işlemleri

--- frontend/src/components/Cart/CartCoupon.jsx
+++ frontend/src/components/Cart/CartCoupon.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { message } from "antd";
 import { CartContext } from "../../context/CartProvider";
 
--- frontend/src/components/Cart/CartTotals.jsx
+++ frontend/src/components/Cart/CartTotals.jsx
@@ -1,9 +1,16 @@
 import { useContext, useEffect, useState } from "react";
 import { CartContext } from "../../context/CartProvider";
+import { message } from "antd";
+import { loadStripe } from "@stripe/stripe-js";
 
 const CartTotals = () => {
+  const stripePublicKey = import.meta.env.VITE_API_STRIPE_PUBLIC_KEY;
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
   const { cartItems } = useContext(CartContext);
   const [checkedCargo, setCheckedCargo] = useState(false);
+  const user = localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : null;
 
   // console.log("total", cartItems);
 
@@ -12,10 +19,47 @@ const CartTotals = () => {
 
     return itemTotal;
   });
-  //? Toplama işlemi
+  //! Toplama işlemi
   const subTotals = cartItemTotals.reduce((previousValue, currentValue) => {
     return previousValue + currentValue;
   }, 0);
+
+  const handlePayment = async () => {
+    if (!user) {
+      message.info("Ödeme yapmak için lütfen giriş yapınız");
+    }
+    const body = {
+      products: cartItems,
+      user: user,
+      cargoFee: checkedCargo ? cargoFee : 0,
+    };
+
+    try {
+      const stripe = await loadStripe(stripePublicKey);
+
+      const res = await fetch(`${apiUrl}/api/payment`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      if (!res.ok) {
+        message.error("Ödeme işlemi yapılamadı !");
+        return;
+      }
+
+      const session = await res.json();
+      const result = await stripe.redirectToCheckout({
+        sessionId: session.id,
+      });
+
+      if (result.error) {
+        throw new Error(result.error.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(body);
+  };
 
   const cargoFee = 15.69;
   const cartTotal = checkedCargo ? cargoFee + subTotals : subTotals;
@@ -61,7 +105,9 @@ const CartTotals = () => {
         </tbody>
       </table>
       <div className="checkout">
-        <button className="btn btn-lg">Proceed to checkout</button>
+        <button className="btn btn-lg" onClick={handlePayment}>
+          Proceed to checkout
+        </button>
       </div>
     </div>
   );
